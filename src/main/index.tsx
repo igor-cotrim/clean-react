7e9dbc9a255db21823dc912e187cd54e4c1cf211
Igor Cotrim <igorxuxicotrim@gmail.com>
2022-05-20 19:31:49 -0300

    feat: add app router

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,13 +1,13 @@
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
 
-import { Login } from '@/presentation/pages'
+import { Router } from '@/presentation/components'
 import { GlobalStyle, theme } from '@/presentation/styles'
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyle />
-    <Login />
+    <Router />
   </ThemeProvider>,
   document.getElementById('main')
 )

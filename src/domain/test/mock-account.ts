f2861653cf3de68fbf8c9bba8fc451d93508886e
Igor Cotrim <igorxuxicotrim@gmail.com>
2022-05-30 17:06:19 -0300

    refactor: update eslint-standard-with-typescript version

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,4 +1,4 @@
-import faker from '@faker-js/faker'
+import { faker } from '@faker-js/faker'
 
 import { AuthenticationParams } from '@/domain/usecases'
 import { AccountModel } from '@/domain/models'
--- src/validation/test/mock-field-validation.ts
+++ src/validation/test/mock-field-validation.ts
@@ -4,7 +4,7 @@ export class FieldValidationSpy implements FieldValidation {
   error: Error = null
   constructor(readonly field: string) {}
 
-  validate(value: string): Error {
+  validate(): Error {
     return this.error
   }
 }

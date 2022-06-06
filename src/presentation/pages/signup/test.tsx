c4234fd7b54cd7a6e513ec247fd512ea97916291
Igor Cotrim <igorxuxicotrim@gmail.com>
2022-06-06 13:37:35 -0300

    test: ensure SignUp enable submit button if form is valid

--- src/presentation/pages/signup/test.tsx
+++ src/presentation/pages/signup/test.tsx
@@ -99,4 +99,15 @@ describe('#SignUp', () => {
     Helper.populateField(sut, 'passwordConfirmation')
     Helper.testStatusForField(sut, 'passwordConfirmation')
   })
+
+  it('should enable submit button if form is valid', () => {
+    const { sut } = makeSut()
+
+    Helper.populateField(sut, 'name')
+    Helper.populateField(sut, 'email')
+    Helper.populateField(sut, 'password')
+    Helper.populateField(sut, 'passwordConfirmation')
+
+    Helper.testButtonIsDisabled(sut, 'submit', false)
+  })
 })

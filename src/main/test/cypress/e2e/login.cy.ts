b226a8dd9cde34c4b7c6a2c60de0b3f270aa988b
Igor Cotrim <igorxuxicotrim@gmail.com>
2022-06-08 16:29:02 -0300

    test: ensure login begin with correct initial state

--- src/main/test/cypress/e2e/login.cy.ts
+++ src/main/test/cypress/e2e/login.cy.ts
@@ -1,5 +1,19 @@
 describe('Login', () => {
-  it('should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000/login')
+  })
+
+  it('should load with correct initial state', () => {
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴')
+
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴')
+
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    
+    cy.getByTestId('error-wrapper').should('not.have.descendants')
   })
 })

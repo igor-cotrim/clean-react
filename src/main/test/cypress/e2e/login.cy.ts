7171b707a8933c247481f930ca4ea4938524d6ee
Igor Cotrim <igorxuxicotrim@gmail.com>
2022-06-09 20:23:40 -0300

    test: prevent Login to call submit multiple times

--- src/main/test/cypress/e2e/login.cy.ts
+++ src/main/test/cypress/e2e/login.cy.ts
@@ -133,4 +133,22 @@ describe('Login', () => {
     cy.url().should('eq', `${baseUrl}/`)
     cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
   })
+  
+  it('should present multiple submits', () => {
+    cy.intercept({
+      method: 'POST',
+      url: /login/,
+    }, {
+      statusCode: 200,
+      body: {
+        accessToken: faker.datatype.uuid()
+      }
+    }).as('request')
+
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').dblclick()
+    
+    cy.get('@request.all').should('have.length', 1)
+  })
 })

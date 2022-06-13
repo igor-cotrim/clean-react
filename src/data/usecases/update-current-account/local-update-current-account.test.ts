68f3475fa21810cabba858498d4da635e64f7e37
Igor Cotrim <igorxuxicotrim@gmail.com>
2022-06-13 16:44:24 -0300

    refactor: remove async to LocalStorage

--- src/data/usecases/update-current-account/local-update-current-account.test.ts
+++ src/data/usecases/update-current-account/local-update-current-account.test.ts
@@ -33,7 +33,9 @@ describe('#LocalUpdateCurrentAccount', () => {
   it('should throw if SetStorage throws', async () => {
     const { sut, setStorageMock } = makeSut()
 
-    jest.spyOn(setStorageMock, 'set').mockRejectedValueOnce(new Error())
+    jest.spyOn(setStorageMock, 'set').mockImplementationOnce(() => {
+      throw new Error()
+    })
     const promise = sut.save(mockAccountModel())
 
     expect(promise).rejects.toThrow(new Error())

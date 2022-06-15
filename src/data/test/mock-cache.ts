01ec38decce4bf53884b0db4c4f9b20f6521de12
Igor Cotrim <igorxuxicotrim@gmail.com>
2022-06-15 16:40:10 -0300

    test: ensure AuthorizeHttpGetClientDecorator class GetStorage with correct value

---
+++ src/data/test/mock-cache.ts
@@ -0,0 +1,14 @@
+import { faker } from '@faker-js/faker'
+
+import { GetStorage } from '../protocols/cache'
+
+export class GetStorageSpy implements GetStorage {
+  key: string
+  value = faker.datatype.json()
+
+  get(key: string): any {
+    this.key = key
+
+    return this.value
+  }
+}

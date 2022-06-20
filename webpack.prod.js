bf96f2d31d967f5634e9ea0793cebd99cb967244
Igor Cotrim <igorxuxicotrim@gmail.com>
2022-06-20 13:24:42 -0300

    chore: add webpack prod and dev configs

---
+++ webpack.prod.js
@@ -0,0 +1,51 @@
+const { DefinePlugin } = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const { merge } = require('webpack-merge')
+
+const common = require('./webpack.common')
+
+module.exports = merge(common, {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+  externals: {
+    react: 'React',
+    axios: 'axios',
+    recoil: 'Recoil',
+    'react-dom': 'ReactDOM',
+    'react-router-dom': 'ReactRouterDOM'
+  },
+  plugins: [
+    new DefinePlugin({
+      'process.env.API_URL': JSON.stringify('https://fordevs.herokuapp.com/api')
+    }),
+    new HtmlWebpackPlugin({
+      template: './template.prod.html'
+    })
+  ]
+})

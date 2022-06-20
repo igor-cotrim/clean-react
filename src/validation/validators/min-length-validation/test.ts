9cb83805e9bf7e0a387ade392f4bef64e7529f1e
Igor Cotrim <igorxuxicotrim@gmail.com>
2022-06-20 11:55:21 -0300

    fix: change test to ensure values are different

--- src/validation/validators/min-length-validation/test.ts
+++ src/validation/validators/min-length-validation/test.ts
@@ -25,9 +25,9 @@ describe('#MinLengthValidation', () => {
   })
 
   it('should return falsy if field does not exists in schema', () => {
-    const sut = makeSut(faker.database.column())
+    const sut = makeSut('any_field')
     const error = sut.validate({
-      [faker.database.column()]: faker.random.alphaNumeric(5)
+      invalid_field: faker.random.alphaNumeric(5)
     })
 
     expect(error).toBeFalsy()

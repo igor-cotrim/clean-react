e0d75bd6782aebc07ca7bd06a24cba31fd2715cc
Igor Cotrim <igorxuxicotrim@gmail.com>
2022-06-27 17:09:39 -0300

    test: ensure SurveyResult present correct initial state

--- src/presentation/pages/survey-result/test.tsx
+++ src/presentation/pages/survey-result/test.tsx
@@ -1,3 +1,26 @@
-// describe('#SurveyList', () => {
-//   it('should ', () => {})
-// })
+import { render, screen } from '@/presentation/utils/test-utils'
+
+import { mockAccountModel } from '@/domain/test'
+import { SurveyResult } from '@/presentation/pages'
+import { ApiContext } from '@/presentation/contexts'
+
+describe('#SurveyResult', () => {
+  it('should present correct initial state', () => {
+    render(
+      <ApiContext.Provider
+        value={{
+          setCurrentAccount: jest.fn(),
+          getCurrentAccount: () => mockAccountModel()
+        }}
+      >
+        <SurveyResult />
+      </ApiContext.Provider>
+    )
+
+    const surveyResult = screen.getByTestId('survey-result')
+
+    expect(surveyResult.childElementCount).toBe(0)
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument()
+    expect(screen.queryByTestId('loading')).not.toBeInTheDocument()
+  })
+})
